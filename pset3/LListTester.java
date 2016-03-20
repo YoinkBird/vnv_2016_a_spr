11ffd0b91fd290db0cd96a6f0fb0bb03d46276d9
yoinkbird <YoinkBird@users.noreply.github.com>
2016-03-19 20:50:08 -0500

    implemented addLast and corresponding test

--- pset3/LListTester.java
+++ pset3/LListTester.java
@@ -84,13 +84,31 @@ public class LListTester{
     assertTrue(l.repOk());
   }
 
+  @Test public void addLast_test_toString() {
+    LList l = new LList();
+    int values[] = new int[]{0,1,2,3};
+    String expected = "0 1 2 3";
+    for(int n : values){
+      System.out.println("adding " + n);
+      l.addLast(n);
+    }
+    System.out.println("LList.toString:[" + l.toString() + "]");
+    System.out.println("LList.repr:" + l.repr());
+
+    assertEquals(expected, l.toString());
+    assertTrue(values.length == l.size);
+    assertTrue(l.repOk());
+  }
+
   // pset2a JUnit Tests [6 points]
   /*
     Write some JUnit tests such that each test makes exactly one invocation of addLast
     and running all the tests provides full statement coverage for the method addLast
   */
+  @Ignore
   @Test public void test0(){
   }
+  @Ignore
   @Test public void test1(){
   }
 
