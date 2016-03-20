0c7e7ed3990a8b90b1e1ece7cd0d9bd8028e52ca
yoinkbird <YoinkBird@users.noreply.github.com>
2016-03-19 20:03:34 -0500

    updating build+run script

--- pset3/run.sh
+++ pset3/run.sh
@@ -4,6 +4,10 @@ java_target_file="LListTester.java";
 javac_files="LList.java $java_target_file"
 java_target=`basename -s .java $java_target_file`;
 javac $javac_files;
+if [ $? -ne 0 ]; then
+  echo "javac failed";
+  exit;
+fi
 # $CLASSPATH should already be set; no need for 'java -cp path:path' 
 java org.junit.runner.JUnitCore $java_target;
 
