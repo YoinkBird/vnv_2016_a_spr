68e1eb044914b4349254729e233bad3c245717b0
yoinkbird <YoinkBird@users.noreply.github.com>
2016-03-22 20:09:03 -0500

    enhancing example symbc

--- jpf_learn/examples/Example.java
+++ jpf_learn/examples/Example.java
@@ -1,12 +1,18 @@
 package examples;
+/*
+ * run:
+javac -g examples/Example.java; java -jar ~/workspace/jpf/jpf-core/build/RunJPF.jar examples/Example.jpf 
+ */
 
 public class Example {
 
   public static void main (String[] args) {
     Example ex = new Example();
     ex.foo(2, 1);
+    ex.addLast(1);
   }
 
+  // simple conditional
   public int foo(int x, int y){
     if (x > y){
       System.out.println("First");
@@ -15,6 +21,27 @@ public class Example {
       System.out.println("Second");
       return y;
     }
+  } // end conditional
+
+  // demonstrate while-loop
+  public void addLast(int x) {
+    int y = 2;
+    // adds a new node with element x at the *tail* of the list
+    // all other list nodes remain unchanged
+
+    if(x>1){
+      System.out.println("5");
     }
+    // iterate
+    while(true){
+      if (x > y){
+        System.out.println("First");
+        break;
+      }else{
+        System.out.println("Second");
+        break;
+      }
+    }
+  }
 
 }
