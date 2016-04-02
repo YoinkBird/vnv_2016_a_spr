666dbc0e9adc50ddea5fda2533264ea7909ccef1
yoinkbird <YoinkBird@users.noreply.github.com>
2016-04-02 11:09:14 -0500

    easy part

--- junit/SortTester.java
+++ junit/SortTester.java
@@ -3,11 +3,32 @@ import static org.junit.Assert.*;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.Arrays;
 
 import org.junit.Test;
 
 public class SortTester{
-  @Test
+  //question a
+  @Test (expected=IllegalArgumentException.class)
   public void t0() {
+    Sort sortInst = new Sort();
+    sortInst.sort(null);
   }
+
+  // question b
+  @Test public void t1() {
+    int arr[] = new int[]{};
+    System.out.println("array 0 elem:" + Arrays.toString(arr));
+    Sort.sort(arr);
+    assertTrue(arr.length == 0);
+  }
+
+  // question c
+  @Test public void t2() {
+    int arr[] = new int[]{1};
+    System.out.println("array 1 elem:" + Arrays.toString(arr));
+    Sort.sort(arr);
+    assertTrue(arr.length == 1);
+  }
+
 }
